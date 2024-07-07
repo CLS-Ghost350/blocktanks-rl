669782892937fe191136b13ca592f717d37d821a
Sleepyleaf58 <frank.huang2@student.tdsb.on.ca>
2024-07-07 15:58:44 -0400

    Changed training

--- train2.py
+++ train2.py
@@ -1,8 +1,11 @@
 import os
-from SimpleEnv.BlocktanksGame import BlocktanksEnv
-from stable_baselines3 import PPO
+
+from sb3_plus import MultiOutputPPO
+
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
+
+from BlocktanksEnv import BlocktanksEnv
 
 import math
 
@@ -18,12 +21,14 @@ def create_env(seed):
     return init
 
 envs_num = 6
+total = 2000
 
 if __name__ == "__main__":
-    #env = BlocktanksEnv()
-    env = SubprocVecEnv([ create_env(seed) for seed in range(envs_num) ])
+    for i in range(int(total/200)):
+        env = BlocktanksEnv()
+        #env = SubprocVecEnv([ create_env(seed) for seed in range(envs_num) ])
 
-    model = PPO.load(model_path, env=env, learning_rate=0.0001)
+        model = MultiOutputPPO("MultiOutputPolicy", env, verbose=1, tensorboard_log=log_path, learning_rate=lambda amountLeft: 0.0004 * amountLeft**2)#, learning_rate=0.5)#, n_steps=3500)
 
-    model.learn(total_timesteps=2*1000*1000) #log_interval=100)
-    model.save(model_path)
+        model.learn(total_timesteps=200)#1000)#1.75*1000*1000) #log_interval=100)
+        model.save(model_path)
