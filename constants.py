e5d757f33fb6e73c773a64fa6f7a1d48800c296a
CLS-Ghost350 <jennifer.f.dan@gmail.com>
2024-07-22 08:19:18 -0400

    checkpoint callback

---
+++ constants.py
@@ -0,0 +1,10 @@
+class HyperParameters:
+    N_STEPS = 640
+    LEARNING_RATE = lambda amountLeft: 0.0004 * amountLeft**2
+
+class LearningParameters:
+    TOTAL_TIMESTEPS = 2 * 1000 * 1000 # fps around 18 per env (108 total)
+    N_ENVS = 6
+
+    SAVE_FREQ = 12000
+    SAVE_FREQ = max(SAVE_FREQ // N_ENVS, 1)
